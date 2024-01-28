ff11c53997b6ff662fa173afbace11c6dc165b7f
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2024-01-28 19:04:17 +0500

    Final Commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,8 +12,11 @@ import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSyst
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import {Link} from "react-router-dom"
+import {useContext} from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Sidebar = () => {
+  const {dispatch} = useContext(DarkModeContext); 
   return (
     <div className="sidebar">
       <div className="top">
@@ -32,18 +35,22 @@ const Sidebar = () => {
             </span>
           </li>
           <p className="title">LISTS</p>
+        <Link to="/users" style={{textDecoration:"none"}}>
           <li>
           <PersonOutlineIcon className="icon"/>
             <span>
               Users
             </span>
           </li>
+         </Link>
+         <Link to="/products" style={{textDecoration:"none"}}>
           <li>
             <StoreIcon className="icon"/>
             <span>
               Products
             </span>
           </li>
+          </Link>
           <li>
             <CreditCardIcon className="icon"/>
             <span>
@@ -104,8 +111,8 @@ const Sidebar = () => {
         </ul>
       </div>
       <div className="bottom">
-        <div className="colorOption"></div>
-        <div className="colorOption"></div>
+        <div className="colorOption" onClick={()=> dispatch({type:"LIGHT"})}></div>
+        <div className="colorOption" onClick={()=> dispatch({type:"DARK"})}></div>
       </div>
     </div>
   )
