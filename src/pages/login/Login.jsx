a14c9cd9c225705e1834909c158c02e40a54d778
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2024-01-30 00:50:21 +0500

    new commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./login.scss";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../../firebase"
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navitage = useNavigate()
+
+  
+
+
+  const handleLogin =(e)=>{
+     e.preventDefault();
+     signInWithEmailAndPassword(auth, email, password)
+     .then((userCredential) => {
+       
+       const user = userCredential.user;
+       navitage("/")
+     })
+     .catch((error) => {
+       setError(true);
+     });
+  }
+
   return (
     <div className="login">
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password"/>
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)} />
+        <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
         <button type="submit">Login</button>
-        <span>Wrong email or Password!</span>
+        {error && <span>Wrong email or Password!</span>}
       </form>
     </div>
   )
