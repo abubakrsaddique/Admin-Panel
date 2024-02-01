d418d5de1052f721c596d22f474d9d78533e80a7
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2024-02-01 23:56:26 +0500

    fresh commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./login.scss";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import {auth} from "../../firebase"
+import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import {AuthContext} from "../../context/AuthContext"
 
 const Login = () => {
   const [error, setError] = useState(false);
@@ -11,32 +12,41 @@ const Login = () => {
 
   const navitage = useNavigate()
 
-  
+  const {dispatch} = useContext(AuthContext)
 
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-  const handleLogin =(e)=>{
-     e.preventDefault();
-     signInWithEmailAndPassword(auth, email, password)
-     .then((userCredential) => {
-       
-       const user = userCredential.user;
-       navitage("/")
-     })
-     .catch((error) => {
-       setError(true);
-     });
-  }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        dispatch({type:"LOGIN", payload:user})
+        navitage("/")
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
 
   return (
     <div className="login">
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)} />
-        <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
-        {error && <span>Wrong email or Password!</span>}
+        {error && <span>Wrong email or password!</span>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
